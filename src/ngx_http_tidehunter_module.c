0132509860659a88d3ccdfa07246bbf05784ed72
Ruoshan Huang <ruoshan612@gmail.com>
2013-12-02 16:32:23 +0800

    move handler back to rewrite phase
    let me figure out which PHASE is better, hold.

--- src/ngx_http_tidehunter_module.c
+++ src/ngx_http_tidehunter_module.c
@@ -68,7 +68,7 @@ static ngx_int_t ngx_http_tidehunter_init(ngx_conf_t *cf){
     ngx_http_tidehunter_filter_init_rule(mcf, cf->pool);
 
     /* initialize the REWRITE handler */
-    rewrite_handler_pt = ngx_array_push(&ccf->phases[NGX_HTTP_ACCESS_PHASE].handlers);
+    rewrite_handler_pt = ngx_array_push(&ccf->phases[NGX_HTTP_REWRITE_PHASE].handlers); /* why REWIRTE PHASE? */
     if(rewrite_handler_pt == NULL){
         return NGX_ERROR;
     }
