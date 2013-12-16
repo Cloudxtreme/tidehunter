e4ea984f5b702241e6a1ccd6c881d42b943259b4
Ruoshan Huang <ruoshan612@gmail.com>
2013-12-16 17:38:54 +0800

    add LFI and RFI (php). but php on CLI isn't working that way.

--- example/app.py
+++ example/app.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python2
 # encoding: utf-8
 
+# this is a web server full of vulnerabilities
+# XSS, SQL injection, Directory Traversal, PHP file include.
+#
+# unfaithfully by ruoshan.
+#
+# INSTALLATION: 
+#   - require sqlite3 and pysqlite for SQL
+#   - require php for PHP file include
+
 import os
+import urllib2
+import subprocess
+from subprocess import PIPE
 import tornado.web
 import tornado.ioloop
 
@@ -12,11 +24,13 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("index.html")
 
 class XssHandler(tornado.web.RequestHandler):
+    # xss attack
     def get(self):
         qstr = self.get_argument("qstr", default=None);
         self.render("xss.html", qstr=qstr)
 
 class SqlHandler(tornado.web.RequestHandler):
+    # sql injection
     def get(self):
         qid = self.get_argument("id");
         rows = database.query(qid);
@@ -25,6 +39,53 @@ class SqlHandler(tornado.web.RequestHandler):
             return
         info = rows[0][1]
         self.render("sql.html", info=info)
+
+class LfiHandler(tornado.web.RequestHandler):
+    # Local File Inclusion
+    def get(self):
+        try:
+            filename = self.get_argument("filename")
+        except:
+            self.render("lfi.html")
+            return
+        if not os.path.isdir(filename):
+            self.write(open(filename, 'r').read())     #give you all the secrets
+        else:
+            raise tornado.web.HTTPError(404)
+
+class PhpHandler(tornado.web.RequestHandler):
+    # Remote File Inclusion, (php)
+    # FIXME: NOT work, php under cli is diff from php behind cgi
+    def get(self):
+        # php file on remote side
+        try:
+            phpurl = self.get_argument("phpurl")
+        except:
+            self.render("php.html")
+            return
+        phpcode = "<?php include(\"%s\") ?>" % phpurl
+        print phpcode
+        p = subprocess.Popen(["php", ], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        stdout, _ = p.communicate(input=phpcode)
+        self.write(stdout)
+
+    def post(self):
+        self.get()
+
+class UploadHandler(tornado.web.RequestHandler):
+    # not used yet
+    def post(self):
+        # execute php code in upload form
+        files = self.request.files
+        for var in files:
+            for httpfile in files[var]:
+                p = subprocess.Popen(["php", ], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+                stdout, _ = p.communicate(input=httpfile.body)
+                self.write(stdout)
+
+class PhpStrHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("<?php phpinfo() ?>")
 
 if __name__ == "__main__":
     settings = {
@@ -36,6 +97,9 @@ if __name__ == "__main__":
         (r"/", MainHandler),
         (r"/xss", XssHandler),
         (r"/sql", SqlHandler),
+        (r"/php", PhpHandler),
+        (r"/phpstr", PhpStrHandler),
+        (r"/lfi", LfiHandler),
         ],
         **settings)
     application.listen(8086)
